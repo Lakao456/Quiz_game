28d02fc8f2070a74cca703562ac144ae413c4ac3
Lakao456 <59497054+Lakao456@users.noreply.github.com>
2020-05-28 21:22:55 +0530

    update q_num when question deleted

--- Quiz-Game_new/Ques-SQL.py
+++ Quiz-Game_new/Ques-SQL.py
@@ -2,59 +2,100 @@ import os
 import json
 import mysql.connector
 
-quizGameDB = mysql.connector.connect(
+quizAppDB = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="aman.singh456",
     database="questions")
-sql = quizGameDB.cursor()
+sql = quizAppDB.cursor()
+line_break = '\n----------------------------------------------------------------------\n'
 
 
-def addQues(subject='maths'):
-    mcq_op, ans_mcq, ans_tf, ans_ow = ["NUll, ", "NUll, ", "NUll, ", "NUll, "], "NULL, ", "NULL,", "NULL"
-    in_question = "'" + input("\nEnter the question statement:-") + "', "
-    types = [None, 'mcq', 'true/false', 'oneWord']
-    in_type = "'" + types[int(input("\n1) MCQ"
-                                    "\n2) True/False"
-                                    "\n3) One Word"
-                                    "\nEnter your choice:: "))] + "', "
+def checkInput(value_type, msg="Enter a value"):
+    while True:
+        value = input(msg + ':: ')
+
+        if type(value_type) == type(1):
+            try:
+                value = int(value)
+                if value not in range(1, value_type + 1):
+                    print("**Invalid input**")
+                else:
+                    break
+            except ValueError:
+                print("**Invalid input**1")
+
+        elif value_type == 'mcq':
+            if value.lower() not in ['a', 'b', 'c', 'd']:
+                print("**Invalid input**")
+            else:
+                break
+
+        elif value_type == 'true/false':
+            if value.lower() not in ['t', 'f']:
+                print("**Invalid input**")
+            else:
+                break
+
+    return value
+
+
+def addQues(subject):
+    mcq_op, ans_mcq, ans_tf, ans_ow = ["NUll", "NUll", "NUll", "NUll"], "NULL", "NULL", "NULL"
+    in_question = input("\nEnter the question statement:-\n")
+
+    types = ['mcq', 'true/false', 'oneWord']
+    in_type = types[checkInput(3, line_break + \
+                               "\n1) MCQ"
+                               "\n2) True/False"
+                               "\n3) One Word"
+                               "\nEnter your choice") - 1]
 
     if 'mcq' in in_type:
+        print(line_break)
         for i in range(4):
-            mcq_op[i] = "'" + input("Enter option" + chr(65 + i) + " ::") + "', "
-        ans_mcq = "'" + input("Enter the answer(a/b/c/d):: ") + "', "
+            mcq_op[i] = "'"+input("Enter option" + chr(65 + i) + " ::")+"'"
+        ans_mcq = "'"+checkInput('mcq', line_break+'Enter the answer(a/b/c/d)')+"'"
     elif 'true/false' in in_type:
-        ans_tf = "'" + input("Enter the answer(t/f):: ") + "', "
+        ans_tf = "'"+checkInput('true/false', line_break+'Enter the answer(t/f)')+"'"
     elif 'oneWord' in in_type:
-        ans_ow = "'" + input("Enter the answer:: ")
+        ans_ow = "'"+input(line_break+"Enter the answer:: ")+"'"
 
     sql.execute("USE questions;")
-    sql.execute("SELECT MAX(Q_num) FROM sci;")
-    print(sql.fetchall()[0])
-    # exe_str = "INSERT INTO " + subject + " VALUES('" + last_index + in_question + in_type + \
-    #           mcq_op[0] + \
-    #           mcq_op[1] + \
-    #           mcq_op[2] + \
-    #           mcq_op[3] + \
-    #           ans_mcq + \
-    #           ans_tf + \
-    #           ans_ow + ");"
-    # sql.execute(exe_str)
-    # quizGameDB.commit()
+    sql.execute("SELECT MAX(Q_num) FROM %s;" % subject)
+    q_num = sql.fetchall()[0][0] + 1
+
+    exe_str = "INSERT INTO %s VALUES(%d, '%s', '%s', %s, %s, %s, %s, %s, %s, %s);"\
+              % (subject, q_num, in_question, in_type, mcq_op[0], mcq_op[1], mcq_op[2], mcq_op[3], ans_mcq, ans_tf, ans_ow)
+
+    sql.execute(exe_str)
+    quizAppDB.commit()
 
 
-def delQues(subject='maths'):
+def delQues(subject):
+    sql.execute("USE questions;")
+    sql.execute("SELECT MAX(Q_num) FROM %s;" % subject)
+    lastIndex = sql.fetchall()[0][0]
     while True:
-        print("Enter the question number to delete or enter '0' to view all the questions")
-        in_qNum = int(input(":: "))
+        in_qNum = int(input(line_break+"Enter the question number to delete or enter '0' to view all the questions:: "))
         if in_qNum == 0:
             sql.execute("USE questions;")
-            sql.execute("SELECT Q_num, question FROM " + subject + ";")
+            sql.execute("SELECT Q_num, question FROM %s ;" %subject)
             for p in sql.fetchall(): print(p)
-        else:
+        elif lastIndex >= in_qNum > 0:
             sql.execute("USE questions;")
-            sql.execute("DELETE FROM " + subject + " WHERE Q_num = " + str(in_qNum) + ";")
-            quizGameDB.commit()
+            sql.execute("DELETE FROM %s WHERE Q_num = %d ;" % (subject, in_qNum))
+            for i in range(in_qNum+1, lastIndex+1):
+                x = i - 1
+                sql.execute("UPDATE %s SET Q_num = %d WHERE Q_num = %d ;" % (subject, x, i))
+
+            sql.execute("USE questions;")
+            sql.execute("SELECT Q_num, question FROM %s ;" % subject)
+            for p in sql.fetchall(): print(p)
+
+            quizAppDB.commit()
+            break
+        else: print("**Invalid input**")
 
 
 def modQues(subject='maths'):
@@ -63,43 +104,43 @@ def modQues(subject='maths'):
         in_qNum = input(":: ")
         if in_qNum == 0:
             sql.execute("USE questions;")
-            sql.execute("SELECT Q_num, question FROM " + subject + ";")
+            sql.execute("SELECT Q_num, question FROM %s ;" % subject)
             for p in sql.fetchall(): print(p)
         else:
             sql.execute("USE questions;")
-#NICE
-            sql.execute("SELECT type FROM " + subject + " WHERE Q_num = " + in_qNum + ";")
+            sql.execute("SELECT type FROM %s WHERE Q_num = %d ;" % subject, in_qNum)
+            # sql.execute("SELECT type FROM " + subject + " WHERE Q_num = " + in_qNum + ";")
             type = str(sql.fetchall()[0][0])
 
             if type == 'mcq':
-                options = [None, 'question', 'optionA', 'optionB', 'optionC', 'optionD', 'ansMcq']
+                options = ['question', 'optionA', 'optionB', 'optionC', 'optionD', 'ansMcq']
                 in_option = options[int(input("\n1) Question Statement"
                                               "\n2) Option A"
                                               "\n3) Option B"
                                               "\n4) Option C"
                                               "\n5) Option D"
                                               "\n6) Answer"
-                                              "\nEnter your choice:: "))] + " = '"
+                                              "\nEnter your choice:: "))-1] + " = '"
                 new_value = input("Enter the new value:: ")
             elif type == 'true/false':
-                options = [None, 'question', 'ansTf']
+                options = ['question', 'ansTf']
                 in_option = options[int(input("\n1) Question Statement"
                                               "\n2) Answer"
-                                              "\nEnter your choice:: "))] + " = '"
-                tf = [None, 'true', 'false']
+                                              "\nEnter your choice:: "))-1] + " = '"
+                tf = ['true', 'false']
                 new_value = tf[int(input("\n1) True"
                                          "\n2) False"
-                                         "\nSelect the New value:: "))]
+                                         "\nSelect the New value:: "))-1]
             else:
-                options = [None, 'question', 'ansOw']
+                options = ['question', 'ansOw']
                 in_option = options[int(input("\n1) Question Statement"
                                               "\n2) Answer"
-                                              "\nEnter your choice:: "))] + " = '"
+                                              "\nEnter your choice:: "))-1] + " = '"
                 new_value = input("Enter the new value:: ")
 
             sql.execute("USE questions;")
             sql.execute("UPDATE " + subject + " SET " + in_option + new_value + "' WHERE Q_num = " + in_qNum + ";")
-            quizGameDB.commit()
+            quizAppDB.commit()
 
         if 'n' in input("Modify another value? (y/n):: ").lower():
             break
@@ -110,52 +151,58 @@ def updateJson():
     fields = ['question', 'type', 'optionA', 'optionB', 'optionC', 'optionD', 'ansMcq', 'ansTf', 'ansOw', ]
     for element in range(1, len(fields)):
         fields_str += (', ' + str(fields[element]))
+    print(fields_str)
 
     for subject in ['maths', 'sci', 'gk']:
         questions[subject] = []
         sql.execute("USE questions;")
-        sql.execute("SELECT MAX(Q_num) FROM %s;" %subject)
+        sql.execute("SELECT MAX(Q_num) FROM %s;" % subject)
 
-        for q_num in range(sql.fetchall()[0][0]):
-            print(subject, q_num)
+        for q_num in range(1, sql.fetchall()[0][0]+1):
             single_ques = {}
             sql.execute("USE questions;")
-            sql.execute("SELECT question%s  FROM %s WHERE q_num = %d;" % (fields_str, subject, q_num + 1))
+            sql.execute("SELECT question %s  FROM %s WHERE q_num = %d;" % (fields_str, subject, q_num))
             field_outputs = sql.fetchall()[0]
             if field_outputs[1] == 'mcq':
                 for i in range(7):
                     single_ques[fields[i]] = field_outputs[i]
             elif field_outputs[1] == 'true/false':
-                for i in [0,1,-2]:
+                for i in [0, 1, -2]:
                     single_ques[fields[i]] = field_outputs[i]
             elif field_outputs[1] == 'oneWord':
-                for i in [0,1,-1]:
+                for i in [0, 1, -1]:
                     single_ques[fields[i]] = field_outputs[i]
             questions[subject].append(single_ques)
 
-    with open("questionsTEMP.json", 'w') as tempFile:
+    with open("QuestionsTEMP.json", 'w') as tempFile:
         json.dump(questions, tempFile, indent=4)
 
+    os.remove("Questions.json")
+    os.rename(r'QuestionsTEMP.json', r'Questions.json')
 
-# subs = [None, 'maths', 'sci', 'gk','exit']
-# subject = subs[int(input("\n1) Maths"
-#                          "\n2) Science"
-#                          "\n3) GK"
-#                          "\n4) Exit"
-#                          "\nEnter your choice:: "))]
-# print("---------------------------------------------------")
-# menu = int(input("1) Add Question\n"
-#                  "2) Delete Question\n"
-#                  "3) Modify Question\n"
-#                  "Enter your choice:: "))
-# print("---------------------------------------------------")
-# if menu == 1:
-#     addQues(subject)
-# elif menu == 2:
-#     delQues(subject)
-# elif menu == 3:
-#     modQues(subject)
-# else:
-#     updateJson()
+
+while True:
+    subs = ['maths', 'sci', 'gk']
+    subject = subs[checkInput(3, line_break + \
+                              "\n1) Maths"
+                              "\n2) Science"
+                              "\n3) GK"
+                              "\nEnter your choice") - 1]
+
+    menu = checkInput(4, line_break + \
+                      "\n1) Add Question"
+                      "\n2) Delete Question"
+                      "\n3) Modify Question"
+                      "\n4) Exit"
+                      "\nEnter your choice")
+
+    if menu == 1:
+        addQues(subject)
+    elif menu == 2:
+        delQues(subject)
+    elif menu == 3:
+        modQues(subject)
+
+    if input(line_break + 'Do you want to EXIT(y/n)::').lower() in 'yes': break
 
 updateJson()
